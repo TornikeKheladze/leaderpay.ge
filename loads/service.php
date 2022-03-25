fc3b75b84a4e8462eed5aa4003b8d5ce383f84ac
97lazare <97lazare@gmail.com>
2022-03-25 17:03:35 +0400

    add agent and agent_id filed to user balance history

--- loads/service.php
+++ loads/service.php
@@ -348,6 +348,8 @@ if (isset($_GET['action']) && $_GET['action'] == "info") {
                                 "unicdate" => $unicdate,
                                 "balance" => $new_balance,
                                 "description" => $description,
+                                "agent_id" => 2,
+                                "agent" => 1,
                             );
                             $insert_details = $db->insert("user_balance_history", $history_params);
                             $pay_params = array();
