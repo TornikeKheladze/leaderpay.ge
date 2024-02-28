d551c20513eaea52860cd40d4bbef0c1f6002752
tornikeKheladze <xeladze.tornike@gmail.com>
2024-02-28 17:07:44 +0400

    fix:baseUrl

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://dev.back.leaderpay.ge/api",
+  baseURL: "https://api.apw.ge/walletApi/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
